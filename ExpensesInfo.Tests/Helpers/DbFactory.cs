4b5ac37085a7ea50c4931190378265b8cf7ed92c
PC K101 <PC K101@K1-01>
2025-12-04 11:29:33 +0200

    tests added

---
+++ ExpensesInfo.Tests/Helpers/DbFactory.cs
@@ -0,0 +1,18 @@
+﻿using ExpensesInfo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Xml.Linq;
+namespace ExpensesInfo.Tests.Helpers
+{
+    public static class DbFactory
+    {
+        public static ExpensesInfoDbContext CreateInMemory(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<ExpensesInfoDbContext>()
+.UseInMemoryDatabase(dbName)
+.Options;
+            return new ExpensesInfoDbContext(options);
+        }
+
+    }
+}
